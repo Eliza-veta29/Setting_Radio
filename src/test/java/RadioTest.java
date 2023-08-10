f6a1308c3be53fbab2d389e97b7809a38f753602
Liza <skomorohova-elizaveta@rambler.ru>
2023-08-10 16:33:20 +0300

    RadioTest

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,8 +1,7 @@
 import org.example.Radio;
 import org.junit.jupiter.api.Assertions;
-        import org.junit.jupiter.api.Test;
-        import org.junit.jupiter.params.ParameterizedTest;
-        import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RadioTest {
     @ParameterizedTest
